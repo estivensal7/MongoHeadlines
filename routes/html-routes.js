e549f5607f4ab0e3fd159c1dbc48cb07b5b1652a
unknown <estivensal7@gmail.com>
2018-05-01 23:35:19 -0400

    routes directory is set

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+const cheerio = require("cheerio");
+const request = require("request");
+const db = require("../models");
+
+module.exports = function(app) {
+	app.get("/", function(req, res) {
+		db.Article.find().sort({ created: -1 })
+		.then(function(results) {
+			res.render("index", {results : results});
+		})
+	})
+
+	app.get("/savedArticles", function(req, res) {
+		db.Article.find( {saved: true} ).sort( {created: 1} )
+		.then(function(results) {
+			res.render("savedArticles", {results: results});
+		})
+	})
+}
